6a73818a963302eaeb8a1c71234cbef1d4e827f5
Mark Wienk <mark@wienkit.nl>
2016-10-07 14:09:22 +0200

    Use custom delivery period

---
+++ upgrade/install-1.2.0.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * NOTICE OF LICENSE
+ *
+ * This file is licenced under the Software License Agreement.
+ * With the purchase or the installation of the software in your application
+ * you accept the licence agreement.
+ *
+ * You must not modify, adapt or create derivative works of this source code
+ *
+ *  @author    Mark Wienk
+ *  @copyright 2013-2016 Wienk IT
+ *  @license   LICENSE.txt
+ */
+
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
+
+function upgrade_module_1_2_0($object)
+{
+    return (
+        Db::getInstance()->execute(
+            'ALTER TABLE `'._DB_PREFIX_.'beslist_product` ADD `delivery_code_nl` VARCHAR(255) AFTER `status`'
+        ) &&
+        Db::getInstance()->execute(
+            'ALTER TABLE `'._DB_PREFIX_.'beslist_product` ADD `delivery_code_be` VARCHAR(255) AFTER `delivery_code_nl`'
+        )
+    );
+}
