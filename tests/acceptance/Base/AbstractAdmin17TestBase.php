3a4b952dab246965bc0ec463985e03b2548b81f1
Mark Wienk <mark@wienkit.nl>
2017-09-10 21:41:10 +0200

    Use fallback nav-bar class

--- tests/acceptance/Base/AbstractAdmin17TestBase.php
+++ tests/acceptance/Base/AbstractAdmin17TestBase.php
@@ -5,7 +5,12 @@ abstract class AbstractAdmin17TestBase extends ATestBase
 {
     public function goToMenu(array $items)
     {
-        $nav = $this->driver->findElement(\WebDriverBy::id('nav-sidebar'));
+        try {
+            $nav = $this->driver->findElement(\WebDriverBy::id('nav-sidebar'));
+        } catch (\NoSuchElementException $e) {
+            $nav = $this->driver->findElement(\WebDriverBy::className('.nav-bar'));
+        }
+
         $trail = $nav;
         foreach ($items as $item) {
             $trail = $nav->findElement(\WebDriverBy::linkText($item));
