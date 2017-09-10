cca7a4bde935a1e4b91d96c50bf47228a080f20e
Mark Wienk <mark@wienkit.nl>
2017-09-10 11:29:49 +0200

    Test retrieve orders

--- tests/acceptance/SetupModuleTest.php
+++ tests/acceptance/SetupModuleTest.php
@@ -25,6 +25,9 @@ class SetupModuleTest extends BaseTest
         $this->goToPath('index.php?controller=AdminModules');
         $this->goToPath('index.php?controller=AdminModules&install=beslistcart&tab_module=market_place&module_name=beslistcart');
         $this->assertContains("Installatie module(s) geslaagd", $this->getStatusMessageText());
+
+        $this->goToPath('index.php?controller=AdminCountries&id_country=3&statuscountry');
+        $this->assertContains('De status is bijgewerkt', $this->getStatusMessageText());
     }
 
     /**
@@ -41,20 +44,35 @@ class SetupModuleTest extends BaseTest
         $this->driver->findElement(\WebDriverBy::cssSelector("label[for='beslist_cart_enabled_nl_on']"))->click();
         $this->driver->findElement(\WebDriverBy::id('beslist_cart_deliveryperiod_nl'))->sendKeys("Op werkdagen voor 15:00 uur besteld, volgende dag in huis!");
         $this->driver->findElement(\WebDriverBy::id('beslist_cart_deliveryperiod_nostock_nl'))->sendKeys("Niet op voorraad");
+        $this->selectOption('beslist_cart_carrier_nl', '2');
 
         $this->driver->findElement(\WebDriverBy::cssSelector("label[for='beslist_cart_enabled_be_on']"))->click();
         $this->driver->findElement(\WebDriverBy::id('beslist_cart_deliveryperiod_be'))->sendKeys("Op werkdagen voor 15:00 uur besteld, volgende dag in huis!");
         $this->driver->findElement(\WebDriverBy::id('beslist_cart_deliveryperiod_nostock_be'))->sendKeys("Niet op voorraad");
+        $this->selectOption('beslist_cart_carrier_be', '2');
 
         $this->driver->findElement(\WebDriverBy::cssSelector("label[for='beslist_cart_enabled_on']"))->click();
         $this->driver->findElement(\WebDriverBy::id('beslist_cart_shopid'))->sendKeys(getenv('SHOP_ID'));
         $this->driver->findElement(\WebDriverBy::id('beslist_cart_clientid'))->sendKeys(getenv('CLIENT_ID'));
         $this->driver->findElement(\WebDriverBy::id('beslist_cart_personalkey'))->sendKeys(getenv('ORDER_XML_KEY'));
         $this->driver->findElement(\WebDriverBy::id('beslist_cart_shopitem_apikey'))->sendKeys(getenv('SHOPITEM_API_KEY'));
-
         $this->driver->findElement(\WebDriverBy::cssSelector("label[for='beslist_cart_testmode_on']"))->click();
+        $this->driver->findElement(\WebDriverBy::id('beslist_cart_test_reference'))->sendKeys('2-1');
         $this->driver->findElement(\WebDriverBy::id('configuration_form'))->submit();
 
         $this->assertContains("Instellingen bijgewerkt", $this->getStatusMessageText());
+    }
+
+    /**
+     * @group 16
+     * @depends testConfigureModule
+     */
+    public function testSyncOrders()
+    {
+        $this->doAdminLogin();
+        $this->goToPath('index.php?controller=AdminBeslistCartOrders');
+        $this->driver->findElement(\WebDriverBy::id('page-header-desc-order-sync_orders'))->click();
+        $this->assertContains('Beslist.nl Shopping cart sync completed', $this->getStatusMessageText());
+        $this->assertContains('T. van TestAchternaam', $this->driver->findElement(\WebDriverBy::id('form-order'))->getText());
     }
 }
